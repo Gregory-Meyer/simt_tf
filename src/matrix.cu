f40204d2eefa2916e6460f4996287c03d0e78ae9
Gregory Meyer <gregjm@umich.edu>
2019-04-17 14:05:20 -0400

    document matrix.{h,cu}

--- src/matrix.cu
+++ src/matrix.cu
@@ -4,6 +4,12 @@
 
 namespace simt_tf {
 
+/**
+ *  Serializes a matrix to a std::ostream.
+ *
+ *  Elements are output in row-major order on a single line, like:
+ *  {{a00, a01, a02}, {a10, a11, a12}, {a20, a21, a22}}.
+ */
 std::ostream& operator<<(std::ostream &os, const Matrix &m) {
     return os << '{' << m[0] << ", " << m[1] << ", " << m[2] << '}';
 }
